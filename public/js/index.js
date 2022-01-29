5500485d1c2de386c703a65a396b6ebe16acb2d0
Silvia Martinez <sil@silvia.com>
2022-01-29 12:28:44 +0100

    Fixed eslint bug

--- public/js/index.js
+++ public/js/index.js
@@ -2,3 +2,5 @@ import PageComponent from "./components/PageComponent.js";
 
 const parentElement = document.querySelector("body");
 const page = new PageComponent(parentElement, "page");
+
+export default page;
