e37d9f55739c35862c1ce72e4f9f4664cbf5c1a5
Silvia Martinez <sil@silvia.com>
2022-01-30 00:41:29 +0100

    Update PokeCard with API data

--- public/js/components/PokeCardComponent.js
+++ public/js/components/PokeCardComponent.js
@@ -3,25 +3,20 @@ import ButtonComponent from "./ButtonComponent.js";
 import Component from "./Component.js";
 
 class PokeCardComponent extends Component {
+  pokemonDetails;
+
   name;
-
-  constructor(parentElement, className, htmlTag, name) {
-    super(parentElement, className, htmlTag);
-
-    this.name = name;
-
-    this.generateHTML();
-  }
 
   generateHTML() {
     this.element.innerHTML = `
-                
-            <div class="card__image"></div>
+            <div class="card__image">
+            <img src="${this.pokemonDetails.sprites.other["official-artwork"].front_default}"/></div>
             <div class="card__text">
-              <h2 class="card__text--title">Nombre: ${this.name}</h2>
-              <p class="card__text--abilities">Habilidad:${this.infoArray}</p>
-              <p class="card__text--velocidad">Velocidad${this.infoArray}</p>
-              <p class="card__text--peso">Peso</p>
+              <h2 class="card__text--title">Nombre: ${this.pokemonDetails.name}</h2>
+              <p class="card__text--abilities">HP: ${this.pokemonDetails.stats[0].base_stat}</p>
+              <p class="card__text--velocidad">Experiencia: ${this.pokemonDetails.base_experience}</p>
+              <p class="card__text--peso">Peso: ${this.pokemonDetails.weight}</p>
+            <div class="button__container"></div>
             </div>`;
   }
 }
