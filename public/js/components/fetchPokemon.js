1971a76ad296da6716d7450e2807a915d27cd97b
Silvia Martinez <sil@silvia.com>
2022-01-30 20:28:50 +0100

    Fixed path bug in fetchPokemon.js

--- public/js/components/fetchPokemon.js
+++ public/js/components/fetchPokemon.js
@@ -1,22 +1,22 @@
 /* eslint-disable no-unused-vars */
-let nextPokemons = "";
+const nextPokemons = "";
 const pokeApi = "https://pokeapi.co/api/v2/pokemon/";
 
 async function fetchPokemons() {
-  const url = pokeApi;
-
-  const pokemons = await fetch(url);
+  const pokemons = await fetch("https://pokeapi.co/api/v2/pokemon/");
   const results = await pokemons.json();
 
   const pokemonArray = results;
   return pokemonArray;
 }
 
-export const pokemonList = await fetchPokemons();
-export default nextPokemons = pokemonList.next;
+const pokemonList = await fetchPokemons();
+/* export default nextPokemons = pokemonList.next; */
 
 export async function getPokemon(url) {
   const response = await fetch(url);
   const pokemon = await response.json();
   return pokemon;
 }
+
+export default pokemonList;
